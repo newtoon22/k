67328a8a93c28134b757f48170d6b0d3adaa2378
newtoon22 <djeslaifer@gmail.com>
2018-04-28 19:15:02 -0500

    Add files via upload

---
+++ TO/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script for install the tools.
+
+# SlowLoris https://en.wikipedia.org/wiki/Slowloris_(computer_security)
+
+# GoldenEye https://github.com/jseidl/GoldenEye
+
+# EtherApe GUI network activity https://en.wikipedia.org/wiki/EtherApe
+sudo apt-get --yes install etherape
+sudo chmod +x run.sh
+
+exit
